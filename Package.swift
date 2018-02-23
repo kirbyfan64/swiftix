42b15a23e2a906a41db445b7b7ff46bac47c48ea
Ryan Gonzalez <rymg19@gmail.com>
2018-02-23 10:35:19 -0600

    Update package minimum versions

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "swiftix",
     dependencies: [
-        .package(url: "https://github.com/vapor/console", from: "2.2.0"),
-        .package(url: "https://github.com/Coder-256/Regex", .branch("swift4")),
-        .package(url: "https://github.com/scinfu/SwiftSoup", from: "1.5.0"),
+        .package(url: "https://github.com/vapor/console", from: "2.3.1"),
+        .package(url: "https://github.com/crossroadlabs/Regex", from: "1.1.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup", from: "1.6.1"),
     ],
     targets: [
         .target(name: "swiftix", dependencies: [
